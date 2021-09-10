e36b400f0437f0faee570e7d3d8374b48ddc86d7
maninder416 <Maninder.Singh@fullstacklabs.ca>
2021-09-10 15:45:25 -0400

    Added SchoolType enum

---
+++ src/io/reactivestax/school/SchoolType.java
@@ -0,0 +1,12 @@
+package io.reactivestax.school;
+public enum SchoolType {
+    PRE_SCHOOL("PreSchool"),
+    ELEMENTARY_SCHOOL("ElementarySchool"),
+    MIDDLE_SCHOOL("MiddleSchool"),
+    HIGH_SCHOOL("HighSchool");
+
+    private String school_name;
+    SchoolType(String school_name) {
+        this.school_name=school_name;
+    }
+}
