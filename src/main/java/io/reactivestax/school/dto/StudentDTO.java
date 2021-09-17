3aba901882de85fe2fa6615f079330ca81247d36
robin bajaj <robin@reactivestax.io>
2021-09-17 17:20:36 -0400

    added simple fields for data collection in studentDTO

--- src/main/java/io/reactivestax/school/dto/StudentDTO.java
+++ src/main/java/io/reactivestax/school/dto/StudentDTO.java
@@ -1,4 +1,23 @@
 package io.reactivestax.school.dto;
 
 public class StudentDTO {
+    private String name;
+    private String dateOfBirth;
+
+    public StudentDTO() {
+    }
+
+    public StudentDTO(String name, String dateOfBirth) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentDTO{" +
+                "name='" + name + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                '}';
+    }
+
 }
